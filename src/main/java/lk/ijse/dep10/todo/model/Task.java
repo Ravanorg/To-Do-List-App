887385af2a05506b504e784a2b315b1a8d057e2f
Rasindu Hansika <rasinduhansika17218@gmail.com>
2023-05-04 12:31:52 +0530

    conect with database

---
+++ src/main/java/lk/ijse/dep10/todo/model/Task.java
@@ -0,0 +1,19 @@
+package lk.ijse.dep10.todo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task implements Serializable {
+    int id;
+    String description;
+    Status status;
+
+    public  enum Status{
+        COMPLETED,NOT_COMPLETED
+    }
+}
